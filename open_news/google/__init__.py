098719ff5af2e1cd5b98d8280d2d59638d6ac5a3
Ted Chen <imaging8896@gmail.com>
2025-08-17 14:38:40 +0800

    chore: Fix wrong typing

--- open_news/google/__init__.py
+++ open_news/google/__init__.py
@@ -4,7 +4,7 @@ from ._parser import GoogleNewsHTMLParser
 from .constant import Category, Location
 
 
-def get_news(category: Category, category_id: str, location: Location, section_id: str = None):
+def get_news(category: Category, category_id: str, location: Location, section_id: str | None = None):
     url = _get_google_url(category, category_id, location, section_id=section_id)
     with requests.get(url, timeout=10) as response:
         response.raise_for_status()
@@ -15,7 +15,7 @@ def get_news(category: Category, category_id: str, location: Location, section_i
         return parser.all_news
 
 
-def _get_google_url(category: Category, category_id: str, location: Location, section_id: str = None) -> str:
+def _get_google_url(category: Category, category_id: str, location: Location, section_id: str | None = None) -> str:
     if section_id is None:
         return f"https://news.google.com/{category.value}/{category_id}?{location.value}"
     return f"https://news.google.com/{category.value}/{category_id}/sections/{section_id}?{location.value}"
